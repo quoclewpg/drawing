915d7c2ba3876b268a4567e7a66980780f910f09
YOUR_USERNAME <jojododoe1212@gmail.com>
2019-03-24 00:37:15 -0500

    finished

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup()
+{
+	createCanvas(1000, 1000);
+	background(51);
+}
+
+function draw()
+{
+	stroke(255, 0, 200);
+	strokeWeight(10);
+
+	if (mouseIsPressed === true) 
+	{
+   		line(mouseX, mouseY, pmouseX, pmouseY);
+  	}
+}
